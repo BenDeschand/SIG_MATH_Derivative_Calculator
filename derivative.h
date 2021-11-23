2f7ea5fad79be3e78ef5d1eaeb046d448da25fbe
Dylan <92765041+bruhusername@users.noreply.github.com>
2021-11-22 19:51:29 -0600

    Update derivative.h
    COMMIT MY CHANGES OR SUFFER MY WRATH

--- derivative.h
+++ derivative.h
@@ -17,4 +17,7 @@ class derivCal
         string getString();                   // accessor for string variable (for testing purposes)
         vector<char> getVector();             // accessor for vector variable (for testing purposes)
         char getVar();                        // accessor for var variable (for testing purposes)
+        
+        string powerRule(char var; string s); // power rule (yo mama)
+        vector<int> findMult(string s);       // find multiplication sign
 };
