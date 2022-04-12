11c2a6df70058606ef52da2b2d6e83e2bab09156
BenDeschand <bdesch2@uic.edu>
2022-04-11 20:04:57 -0500

    added spaces around operators

--- test01.cpp
+++ test01.cpp
@@ -20,17 +20,17 @@ int test12();
 int main() {
     cout << endl << "----------  DERIVCAL TESTS ----------" << endl << endl;
 
-    // test01();
-    // test02();
-    // test03();
-    // test04();
-    // test05();
-    // test06();
-    // test07();
-    // test08();
-    // test09();
-    // test10();
-    // test11();
+    test01();
+    test02();
+    test03();
+    test04();
+    test05();
+    test06();
+    test07();
+    test08();
+    test09();
+    test10();
+    test11();
     test12();
 
     cout << "----------  DERIVCAL TESTS ----------" << endl << endl;
